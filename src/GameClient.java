a9040044eca34dd43edea2448ba81bc68cc1e4ec
Zach Lesperance <zplesperance@gmail.com>
2015-05-20 12:15:55 -0700

    Basic GUI

--- src/GameClient.java
+++ src/GameClient.java
@@ -1,23 +1,27 @@
+
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-
-public class GameClient extends Application {	
-	public static void main(String args) {
-		launch(args);		
-	}
+public class GameClient extends Application {
 
 	@Override
-	public void start(Stage stage) throws Exception {
+	public void start(Stage primaryStage) throws IOException {
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("mapWindow.fxml"));
 		
 		Scene scene = new Scene(loader.<Parent>load());
 		
-		stage.setTitle("Dungeon Adventure Quest Fighter IV");
-		stage.setScene(scene);
-		stage.show();
+		primaryStage.setTitle("CSCD349 Final Project");
+		primaryStage.setScene(scene);
+
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
 	}
 }
---
+++ src/MapWindowController.java
@@ -0,0 +1,115 @@
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Label;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+
+
+public class MapWindowController implements Initializable {
+	private final double CELL_SIZE = 48;
+	private int partyX;
+	private int partyY;
+	private GraphicsContext gc;
+	@FXML
+	Canvas mapCanvas;
+	@FXML
+	Label messageBox;
+	@FXML
+	VBox windowVBox;
+
+	@Override
+	public void initialize(URL url, ResourceBundle resourceBundle) {
+		partyX = 0;
+		partyY = 0;
+		
+		this.gc = mapCanvas.getGraphicsContext2D();
+		drawMapGrid(gc);
+		drawParty(gc);
+		
+		messageBox.setText("Game Ready");
+		
+		installKeyEventHandler(windowVBox);
+	}
+	
+	private void updateMap() {
+		gc.clearRect(0, 0, mapCanvas.getWidth(), mapCanvas.getHeight());
+		drawMapGrid(gc);
+		drawParty(gc);
+	}
+	
+	private void drawMapGrid(GraphicsContext gc) {
+		gc.setStroke(Color.BLACK);
+		gc.setLineWidth(2);
+		for (int i = 0; i <= mapCanvas.getWidth(); i += CELL_SIZE) {
+			gc.strokeLine(i, 0, i, mapCanvas.getHeight());
+			gc.strokeLine(0, i, mapCanvas.getWidth(), i);
+		}
+	}
+	
+	private void drawParty(GraphicsContext gc) {
+		int x = partyX;
+		int y = partyY;
+		double padding = 4;
+		double baseX = x * CELL_SIZE, baseY = y * CELL_SIZE;
+		gc.strokePolygon(new double[]{baseX + (CELL_SIZE / 2.0), baseX + padding, baseX + CELL_SIZE - padding}, new double[]{baseY + padding, baseY + CELL_SIZE - padding, baseY + CELL_SIZE - padding}, 3);
+	}
+	
+	private void installKeyEventHandler(final Node keyNode) {
+		final EventHandler<KeyEvent> keyEventHandler = 
+				new EventHandler<KeyEvent>() {
+			public void handle(final KeyEvent keyEvent) {
+				if (keyEvent.getCode() == KeyCode.RIGHT) {
+					messageBox.setText("Right Pressed");
+					tryTravelRight();
+					keyEvent.consume();
+				} else if (keyEvent.getCode() == KeyCode.LEFT) {
+					messageBox.setText("Left Pressed");
+					tryTravelLeft();
+					keyEvent.consume();
+				} else if (keyEvent.getCode() == KeyCode.UP) {
+					messageBox.setText("Up Pressed");
+					tryTravelUp();
+					keyEvent.consume();
+				} else if (keyEvent.getCode() == KeyCode.DOWN) {
+					messageBox.setText("Down Pressed");
+					tryTravelDown();
+					keyEvent.consume();
+				} else {
+					messageBox.setText("Key Pressed");
+				}
+			}
+		};
+
+	    keyNode.setOnKeyReleased(keyEventHandler);
+	}
+	
+	private void tryTravelRight() {
+		partyX++;
+		updateMap();
+	}
+	
+	private void tryTravelLeft() {
+		partyX--;
+		updateMap();
+	}
+	
+	private void tryTravelUp() {
+		partyY++;
+		updateMap();
+	}
+	
+	private void tryTravelDown() {
+		partyY--;
+		updateMap();
+	}
+
+}
